ddc6644c198532fbf963abc311c96a71c09156c2
Gustavo <gcostacoelho2003@gmail.com>
2024-04-01 21:22:59 -0300

    feat: create a consumer project

--- Project.Rabbit.Consumer/Program.cs
+++ Project.Rabbit.Consumer/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System.Text;
+using System.Text.Json;
+using Project.Rabbit.Consumer.src.Models;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var connectionFactory = new ConnectionFactory { HostName = "localhost" };
+
+var conn = connectionFactory.CreateConnection();
+
+var channel = conn.CreateModel();
+
+channel.QueueDeclare(
+    queue: "notifications",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+Console.WriteLine("Aguardando mensagens.");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var bodyReceived = ea.Body.ToArray();
+
+    var messageString = Encoding.UTF8.GetString(bodyReceived);
+
+    var notification = JsonSerializer.Deserialize<Notification>(messageString);
+
+    Console.WriteLine($"Assunto: {notification?.Subject}\n{notification?.Body}");
+};
+
+channel.BasicConsume(queue: "notifications", autoAck: true, consumer: consumer);
+
+Console.ReadLine();
---
+++ Project.Rabbit.Consumer/src/models/Notification.cs
@@ -0,0 +1,6 @@
+namespace Project.Rabbit.Consumer.src.Models;
+public class Notification
+{
+    public string? Subject { get; set; }
+    public string? Body { get; set; }
+}
